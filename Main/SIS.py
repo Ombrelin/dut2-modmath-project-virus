1f154d6ee355e5336578d2da804c5411e172da20
Alexis <alexis.gmatias@gmail.com>
2018-10-30 17:37:47 +0100

    modelisation SIS - termine

--- Main/SIS.py
+++ Main/SIS.py
@@ -28,7 +28,7 @@ class ModeleSIS(Frame):
         
         self.infect = Scale(self.fenetre, orient='horizontal', from_=0, to=1,
                        resolution=0.1, tickinterval=0.1, length=350,
-                       label="Entrez le pourcentage de personnes suceptibles d'être infectées : ")
+                       label="Entrez le pourcentage de personnes infectées : ")
         self.infect.pack()
         self.infect.set(0.5)
         
@@ -50,11 +50,17 @@ class ModeleSIS(Frame):
         self.tNaissance.pack()
         self.tNaissance.set(0.2)
         
-        boutonValider = Button(self.fenetre, text="Valider", command= lambda: self.SIS(self.infect.get(),self.tInfect.get(),self.tGuerison.get(), self.tNaissance.get()))
+        self.tMort = Scale(self.fenetre, orient='horizontal', from_=0, to=1,
+                       resolution=0.1, tickinterval=0.1, length=350,
+                       label="Entrez le taux de morts : ")
+        self.tMort.pack()
+        self.tMort.set(0.2)
+        
+        boutonValider = Button(self.fenetre, text="Valider", command= lambda: self.SIS(self.infect.get(),self.tInfect.get(),self.tGuerison.get(), self.tNaissance.get(), self.tMort.get()))
         boutonValider.pack()
     
     
-    def SIS(self, infectee, tInfection, tGuerison, tNaissance):
+    def SIS(self, infectee, tInfection, tGuerison, tNaissance, tMort):
     
         plt.clf()
         popInfectee = float(infectee)
@@ -62,15 +68,15 @@ class ModeleSIS(Frame):
         
         tauxInfection = float(tInfection)
         
+        tauxMort= float(tMort)
         #nombre jours durant lequel une personne reste infecte
         tauxGuerison = float(tGuerison)
         tauxNaissance = float(tNaissance)
-        popTotale = 50
+        popTotale = 1000
         
         
         popSuceptible = popTotale - popTotale * popInfectee;
         popInfectee = popTotale * popInfectee
-        tNG = tauxNaissance+tauxGuerison
         temps = []
         for i in range(100):
             temps.append(i)
@@ -85,10 +91,10 @@ class ModeleSIS(Frame):
         popT.append(popSuceptible + popInfectee)
         
         for i in range(1,100):
-            popSuceptible = popSuceptible -((tauxInfection/popTotale)*popSuceptible*popInfectee )+(tNG*popInfectee)
-            popS.append(popSuceptible)
+            popSuceptible = popSuceptible -((tauxInfection*popSuceptible*popInfectee)/popTotale)+(tauxGuerison*popInfectee)-(tauxMort*popSuceptible)+(tauxNaissance*popTotale)
+            popS.append(popSuceptible)  
             
-            popInfectee = popInfectee + ((tauxInfection/popTotale)*popS[i-1]*popInfectee) - (tNG* popInfectee)
+            popInfectee = popInfectee  + ((tauxInfection*popS[i-1]*popInfectee)/ popTotale) -(tauxMort*popInfectee)-(tauxNaissance*popInfectee)
             popI.append(popInfectee)
             
             
